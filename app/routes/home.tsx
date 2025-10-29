6bc81e72abadb19ec35326e3a2a2636e59f58ba4
Liam Kinney <kinne351@umn.edu>
2025-10-28 20:46:45 -0500

    Authentication finished

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,10 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
-import {resumes} from "~/constants";
+import {resumes} from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,12 +14,18 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const { auth } = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
   return <main className = "bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
 
       <section className = "main-section">
         <div className = "page-heading py-16">
-            <h1>Track Your Applications & Résumé Ratings</h1>
+            <h1>Track Your Applications & Resume Ratings</h1>
             <h2>Review your submissions and check feedback powered by your LLM of choice.</h2>
         </div>
 
